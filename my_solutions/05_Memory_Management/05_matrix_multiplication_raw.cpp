124bf62c484c6c398545b482a15042b6805e9a3b
SDavenia <davesam00@gmail.com>
2022-12-12 23:37:15 +0100

    Finished matrix class with raw pointers

--- my_solutions/05_Memory_Management/05_matrix_multiplication_raw.cpp
+++ my_solutions/05_Memory_Management/05_matrix_multiplication_raw.cpp
@@ -42,26 +42,61 @@ public:
     template <typename O>
     friend std::ostream& operator<<(std::ostream& os, const CMatrix<O>& C);
 
-    // Copy constructor
+    // Copy constructor CMatrix B(A);
+    CMatrix(const CMatrix& A);
 
     // Copy assingment operator
-
+    CMatrix& operator=(const CMatrix& A);
+    
     // Move constructor
+    CMatrix(CMatrix&& A);
 
     // Move assignment operator
+    CMatrix& operator=(CMatrix&& A);
 };
 
 
 int main()
 {
-    CMatrix<double> A, B;
+    CMatrix<double> A;// B;
     A.read_from_file("A.txt");
-    B.read_from_file("B.txt");
-    std::cout<<A<<std::endl;
-    std::cout<<B<<std::endl;
-    auto C = A*B;
-    std::cout<<C<<std::endl;
-    C.write_to_file("C.txt");
+    //B.read_from_file("B.txt");
+    //std::cout<<A<<std::endl;
+    //std::cout<<B<<std::endl;
+    //auto C = A*B;
+    //std::cout<<C<<std::endl;
+    //C.write_to_file("C.txt");
+
+    // COPY CONSTRUCTOR
+    // D(A); ERROR ALL THE FOLLOWING WORK
+    // CMatrix<int> D(A); Error as we have a double
+    //auto D(A);
+    //CMatrix D{A};
+    //CMatrix D(A);
+    //CMatrix<double> D(A);
+    // CMatrix<double> D = A;
+    //std::cout<<D<<std::endl;
+
+    // COPY ASSINGMENT OPERATOR
+    CMatrix<double> E;
+    E = A;
+
+    // MOVE CONSTRUCTOR
+    CMatrix F{std::move(E)};
+    // CMatrix<double> G = std::move(F)
+
+    // Move assignment operator
+    /*
+    WRONG
+    CMatrix G;
+    G = std::move(F);
+
+    Calss 
+    */
+    CMatrix<double> G;
+    G = std::move(F);
+
+    
 }
 
 // Constructor
@@ -124,7 +159,7 @@ CMatrix<T> CMatrix<T>::operator*(const CMatrix<T>& A) const{
     return C;
 }
 
-
+// print output operator overloaded
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const CMatrix<T>& C){
     os<<"Square matrix of dimension "<<C.size<<std::endl;
@@ -135,4 +170,74 @@ std::ostream& operator<<(std::ostream& os, const CMatrix<T>& C){
         os<<""<<std::endl;
     }
     return os;
+}
+
+
+// Copy constructor
+template <typename T>
+CMatrix<T>::CMatrix(const CMatrix<T>& A){
+    if(this != &A){
+        std::cout<<"copy constructor being called"<<std::endl;
+        size = A.size;
+        if(data != nullptr){
+            delete[] data;
+            data = nullptr;
+        }
+        if(A.data == nullptr){data = nullptr;}
+        else{
+            data = new T[size*size];
+            for(int i = 0; i < size*size; i++){
+                data[i] = A.data[i];
+            }
+        }
+
+    }
+}
+
+// Copy assignment operator
+template <typename T>
+CMatrix<T>& CMatrix<T>::operator=(const CMatrix<T>& A){
+    if(this != &A){
+        std::cout<<"copy assignment operator being called"<<std::endl;
+        size = A.size;
+        if(data != nullptr){
+            delete[] data;
+            data = nullptr;
+        }
+        if(A.data == nullptr){data = nullptr;}
+        else{
+            data = new T[size*size];
+            for(int i = 0; i < size*size; i++){
+                data[i] = A.data[i];
+            }
+        }
+
+    }
+    return *this;
+
+}
+
+// Move costructor operator
+template <typename T>
+CMatrix<T>::CMatrix(CMatrix<T>&& A){
+    if (this != &A){
+        std::cout<<"Move constructor being called"<<std::endl;
+        size = A.size;
+        A.size = 0;
+        data = A.data;
+        A.data = nullptr; //since destructor on A is called automatically
+    }
+}
+
+// Move assignment operator
+template <typename T>
+CMatrix<T>& CMatrix<T>::operator=(CMatrix<T>&& A){
+    if (this != &A){
+        std::cout<<"Move assignment operator being called"<<std::endl;
+        size = A.size;
+        A.size = 0;
+        data = A.data;
+        A.data = nullptr; //since destructor on A is called automatically
+    }
+    return *this;
 }
